27496fdcc1435aebc698fdcd01d1673d02556c73
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-08 06:20:46 +1100

    exploring jsx

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <label className="label" htmlFor="username">
+        Enter Username
+      </label>
+      <input id="username" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
