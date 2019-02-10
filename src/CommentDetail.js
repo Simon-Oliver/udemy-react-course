2c9656e7f9b34dbe0d104ad60c40633780a9951e
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-10 12:55:49 +1100

    components props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
+          <img alt="avatar" src={props.avatar} />
         </a>
         <div className="content">
           <a href="/" className="author">
-            {faker.name.firstName()}
+            {props.author}
           </a>
           <div className="metadata">
-            <span className="date">{faker.date.recent().toDateString()}</span>
+            <span className="date">{props.timeAgo}</span>
           </div>
-          <div className="text">{faker.lorem.sentence()}</div>
+          <div className="text">{props.content}</div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
-  return <CommentDetail />;
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 6:00"
+        content="Architecto dolore culpa at facilis."
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
