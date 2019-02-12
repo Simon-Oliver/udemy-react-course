4a967575dbf76db219bc4fe6cb881032492f785c
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-12 21:35:58 +1100

    season app

---
+++ playground/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 6:00"
+          content="Architecto dolore culpa at facilis."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          timeAgo="Today at 6:00"
-          content="Architecto dolore culpa at facilis."
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
   );
+  return <div>Hello</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
