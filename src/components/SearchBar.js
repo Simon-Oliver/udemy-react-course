577cd59fae26547278ae530c0728e88a9efdc2c1
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-18 07:20:40 +1100

    refactored onSubmit call

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state.searchInput);
+    this.props.onSubmit(this.state.searchInput);
   };
 
   render() {
