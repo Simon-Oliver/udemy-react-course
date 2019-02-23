8c0540372cfa4f37f34498b94c22a09a53fb7b1e
Rhiannon Gschwend <rhiannongschwend@192-168-1-101.tpgi.com.au>
2019-02-23 12:51:39 +1100

    added searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,26 @@
 import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { searchTerm: '' };
+
+  onInputChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui input">
+          <input
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search..."
+            onChange={this.onInputChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
