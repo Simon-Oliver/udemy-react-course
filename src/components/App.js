cf72c0fa4b62e791d207d34400b5fc2d37c34f60
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-24 22:32:22 +1100

    added searchBar and hooked up onChange function

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
