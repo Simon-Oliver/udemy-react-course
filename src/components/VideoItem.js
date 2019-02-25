818b96c123049c85afd2b443d9dd5545349b7b55
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-25 21:38:04 +1100

    added VideoItem component and basic styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,1 +1,21 @@
 import React from 'react';
+
+const VideoItem = ({ video }) => {
+  return (
+    <div className="ui items segment">
+      <div className="item">
+        <a className="ui tiny image">
+          <img src={video.snippet.thumbnails.medium.url} />
+        </a>
+        <div className="content">
+          <a className="header">{video.snippet.title}</a>
+          <div className="description">
+            <p>{video.snippet.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos }) => {
+  console.log(videos);
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
+  return <div>{renderedList}</div>;
+};
+
+export default VideoList;
