3cc1c4e839523ac24b66e2727b608de66fb8af69
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-25 20:38:35 +1100

    connected api

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-<<<<<<< HEAD
-import axios from 'axios';
-=======
->>>>>>> 52f4ae0dde483f27ab5e64e17ae5f820ff4749a2
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
-<<<<<<< HEAD
       <div className="ui container">
-=======
-      <div className="ui container" style={{ marginTop: '10px' }}>
->>>>>>> 52f4ae0dde483f27ab5e64e17ae5f820ff4749a2
-        <SearchBar />
+        <SearchBar onTermSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,46 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-<<<<<<< HEAD
-  state = { term: 'Hello' };
+  state = { term: '' };
 
   onInputChange = e => {
     this.setState({ term: e.target.value });
-=======
-  state = { searchTerm: '' };
+  };
 
-  onInputChange = e => {
-    this.setState({ searchTerm: e.target.value });
->>>>>>> 52f4ae0dde483f27ab5e64e17ae5f820ff4749a2
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onTermSubmit(this.state.term);
   };
 
   render() {
     return (
-<<<<<<< HEAD
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input type="text" value={this.state.term} onChange={this.onInputChange} />
           </div>
         </form>
-=======
-      <div className="ui container">
-        <div className="ui input">
-          <input
-            value={this.state.searchTerm} // Controlled Component?
-            type="text"
-            placeholder="Search..."
-            onChange={this.onInputChange}
-          />
-        </div>
->>>>>>> 52f4ae0dde483f27ab5e64e17ae5f820ff4749a2
       </div>
     );
   }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 52f4ae0dde483f27ab5e64e17ae5f820ff4749a2
 export default SearchBar;
