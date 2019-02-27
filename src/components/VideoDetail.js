bb62f4e7e6870a33576c521955c25ec2ebfdbc73
Simon-Oliver <stauffer.simon@outlook.com>
2019-02-27 18:02:08 +1100

    added VideoDetail basic functionality and styling

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div className="ui segment">
+      <h4 className="ui header">{video.snippet.title}</h4>
+      <p>{video.snippet.description}</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
