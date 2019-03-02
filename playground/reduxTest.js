6f78a39298a997f9c156766157b4b116bf317b97
Simon-Oliver <stauffer.simon@outlook.com>
2019-03-03 10:48:33 +1100

    added last reducer

--- playground/reduxTest.js
+++ playground/reduxTest.js
@@ -21,20 +21,40 @@ const deletePolicy = name => {
   };
 };
 
-const createClaim (name, amountToCollect) => {
-return {
+const createClaim = (name, amountToCollect) => {
+  return {
     type: 'CREAT_CLAIM',
     payload: {
-        name,
-        amountToCollect
+      name,
+      amountToCollect
     }
-}
-}
+  };
+};
 
 // Reducers
-const claimsHistory = (oldListOffClaims, action) => {
-if(action.type === 'CREAT_CLAIM'){
-return [...oldListOffClaims, action.payload]
-}
-return oldListOffClaims;
-}
+const claimsHistory = (oldListOffClaims = [], action) => {
+  if (action.type === 'CREAT_CLAIM') {
+    return [...oldListOffClaims, action.payload];
+  }
+  return oldListOffClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountToCollect;
+  }
+  if (action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amount;
+  }
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === 'CREATE_POLICY') {
+    return [...listOfPolicies, action.payload.name];
+  }
+  if (action.type === 'DELETE_POLICY') {
+    return listOfPolicies.filter(name => name !== action.payload.name);
+  }
+  return listOfPolicies;
+};
