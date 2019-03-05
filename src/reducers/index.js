e4eaa98afc33cc1bcde06bafa5fe10a651d44e31
Simon-Oliver <stauffer.simon@outlook.com>
2019-03-05 16:42:19 +1100

    CreatedStore adn combined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducers = () => {
   return [
     { title: 'Numb', duration: '4:05' },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducers,
+  selectedSong: selectedSongReducer
+});
